8c20478214dc3063bc50379c273034ef9c405ad0
“AbdurRaahimm” <rahim703936@gmail.com>
2025-07-23 13:49:30 +0600

    implement Swagger api documentation

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import v1Routes from "./routes/v1/index.js";
 import v2Routes from "./routes/v2/index.js";
 import errorHandler from "./middleware/errorHandler.js";
 import logger from "./middleware/logger.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +20,10 @@ app.use(logger); // Logger middleware for request logging (Use before routes)
 // Mount versioned routes
 app.use("/api/v1", v1Routes);
 app.use("/api/v2", v2Routes);
+
+// Swagger documentation setup
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Global error handler
 app.use(errorHandler);
